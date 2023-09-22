fe6835fc879573472a2b365fd50853dd0c432bbf
anilbhatt1 <anilaknb@gmail.com>
2023-09-22 20:47:43 +0530

    Added makefile

--- src/train.py
+++ src/train.py
@@ -101,10 +101,10 @@ def train(cfg: DictConfig) -> Tuple[Dict[str, Any], Dict[str, Any]]:
     # so will set like trace: True or script: True as needed
     if cfg.get("script"):
         log.info("Starting to save scripted model........")
-        # If cpu is required use this line
-        # scripted_model = model.cpu().to_torchscript(method="script") 
+        # If cpu is required use this line        
+        scripted_model = model.to('cpu').to_torchscript(method="script") 
         # If gpu is required use this line
-        scripted_model = model.to_torchscript(method="script")
+        # scripted_model = model.to_torchscript(method="script")
         torch.jit.save(scripted_model, f"{cfg.paths.output_dir}/model.scripted.pt")
         log.info(f"Scripted model saved to {cfg.paths.output_dir}/model.scripted.pt")
 
